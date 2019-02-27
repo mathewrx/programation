28dff10ae4ac2975ab10f3357ec0232c72f0767c
Your Name <you@example.com>
2019-02-27 10:50:06 -0300

    Comandos p nao esquecer

---
+++ comandos.py
@@ -0,0 +1,16 @@
+    git clone https://github.com/usuario/repositorio.git
+
+    git add nome_arquivo ou  git add * (p add tudo)
+
+    git commit -m "mensagem"
+
+    git push origin master
+
+    git pull
+
+    git remote update
+    git status
+
+    git merge
+
+    git rm nome_arquivo (p apagar)
